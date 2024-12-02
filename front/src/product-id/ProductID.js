d2e12815289014f932c6ec6018bb1f524611cfe0
TuanLinh1892004 <ntlinh1892004@gmail.com>
2024-12-02 15:29:17 +0700

    add smth

--- front/src/product-id/ProductID.js
+++ front/src/product-id/ProductID.js
@@ -2,16 +2,8 @@ import { Header } from "../header/Header"
 import { App_container } from "./app_container/App_container";
 import { useState, useEffect } from "react";
 import { Footer } from "../footer/Footer";
-import { useRouter } from 'next/router';
 
 export function ProductID({id}) {
-    const router = useRouter();
-
-    // nếu chưa đăng nhập, chuyển sang trang đăng nhập
-    useEffect(() => {
-        const user = localStorage.getItem('user');
-        if (!user || user.length === 0) router.push('/login');
-    }, [])
     
     const [searchText, setSearchText] = useState('');
     
