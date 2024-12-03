a167b7da92efc10df343aa618bfac26ade3aaff9
Nguyen Phuong Linh <nplinh20112003@gmail.com>
2024-12-03 21:48:04 +0700

    Chỉnh lại giao diện

--- front/src/terms-of-sale/termsofsale.js
+++ front/src/terms-of-sale/termsofsale.js
@@ -1,7 +1,13 @@
-// src/terms-of-sale/TermsOfSale.js
+import Link from "next/link"
+
 export function TermsOfSale() {
     return (
         <div className="terms-of-sale-container">
+            <Link href='/'>
+                <button className='back-to-home'>
+                    Quay về trang chủ
+                </button>
+            </Link>
             <h1>Điều khoản mua hàng</h1>
             <p>Chúng tôi vẫn còn nhiều sai sót và luôn mong muốn cố gắng cải thiện dịch vụ của mình. Nếu bạn phát hiện bất kỳ điều gì chưa hợp lý, xin vui lòng góp ý cho chúng tôi. Bạn có thể liên hệ với chúng tôi qua địa chỉ sau: <a href="/about">Giới thiệu</a></p>
             
